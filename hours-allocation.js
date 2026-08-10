ea5ca4eed1af2510a9c629b922d5947afdb13992
sebastiangiampani-create <sebastian.giampani@bue.edu.ar>
2026-08-10 10:36:33 -0300

    Habilitar presupuesto de Ciencias Naturales

--- hours-allocation.js
+++ hours-allocation.js
@@ -5,7 +5,7 @@ export const AREA_HOUR_SUBJECTS = Object.freeze({
   'Matemática': Object.freeze(['matematica']),
   'Lenguas Adicionales': Object.freeze(['lenguas-adicionales']),
   'Ciencias Sociales': Object.freeze(['fec', 'geografia', 'historia', 'economia', 'filosofia']),
-  'Ciencias Naturales': Object.freeze([]),
+  'Ciencias Naturales': Object.freeze(['ciencias-naturales']),
   'Artes': Object.freeze(['artes']),
   'Tecnologías': Object.freeze(['tecnologia-informacion']),
   'Educación Física': Object.freeze(['educacion-fisica']),
