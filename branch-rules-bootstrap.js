a8176194731a159846cf85cbb0ed9e390e9c9fd9
sebastiangiampani-create <sebastian.giampani@bue.edu.ar>
2026-08-13 15:16:59 -0300

    Aplicar estructura de prueba para Tecnologías y Otros formatos

---
+++ branch-rules-bootstrap.js
@@ -0,0 +1,6 @@
+import { AREA_CONFIG } from './pci-model.js';
+
+// Reglas de estructura de la rama de prueba.
+// Se aplican antes de iniciar app.js para que la migración use esta configuración.
+AREA_CONFIG.Tecnologías.count = 8;
+AREA_CONFIG['Otros formatos pedagógicos'].sourceArea = 'Otros formatos pedagógicos';
